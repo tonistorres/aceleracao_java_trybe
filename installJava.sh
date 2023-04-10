cd801a383c58fb9058fb6ebc07784bee22675b0b
Tonis Torres <sisvenda2011@gmail.com>
2023-04-10 09:59:16 -0300

    feat: initial commit aceleração java trybe script shell

---
+++ installJava.sh
@@ -0,0 +1,8 @@
+
+INSTALL_JAVA_MACHINE(){
+# atualização do repositorio 
+sudo apt-get update
+#instalando developer kit java via terminhal no ubuntu
+sudo apt install openjdk-11-jdk
+}
+INSTALL_JAVA_MACHINE
